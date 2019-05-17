cd6ace88111ffa5a3a532a6b2843c0284af83202
MojtabaDinarvand <m.dinarvand2016@gmail.com>
2019-05-18 03:33:15 +0430

    add router

--- Main.py
+++ Main.py
@@ -1,1 +1,9 @@
-print("Hello from windows")
+from huawei_lte_api.Client import Client
+from huawei_lte_api.AuthorizedConnection import AuthorizedConnection
+connection = AuthorizedConnection('http://admin:admin@192.168.1.1/')
+client = Client(connection)
+print(client.device.signal())
+print(client.device.information())
+print(client.diagnosis.diagnose_ping())
+print(client.device.basic_information())
+print(client.diagnosis.diagnose_traceroute())
